e5ec65c2823115d2c64c91b9335a271380ba35fc
Gevorg <qamalyan86@gmail.com>
2026-05-02 18:01:32 +0400

    controller: Triger controller to run engines in app open

--- backend/services/controller/app.py
+++ backend/services/controller/app.py
@@ -5,4 +5,5 @@ from .fast_api_app import fast_api_app
 
 def run_controller():
     with fast_api_app() as app:
+        app.controller.start()
         uvicorn.run(app.app, host="127.0.0.1", port=8000)
